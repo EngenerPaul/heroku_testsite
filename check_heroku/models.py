e3af97aa7ed9e5778fac62af0e50418c89e7387e
EngenerPaul <kpashka@mail.ru>
2021-12-08 06:07:39 +0400

    The site is almost completely transfer. Droped all databases. Added all models and views. Changed urls and admin. Don't added all templates.

--- check_heroku/models.py
+++ check_heroku/models.py
@@ -1,20 +1,56 @@
 from django.db import models
-from django.db.models.base import Model
 from django.urls import reverse
 
-class Post(models.Model):
-    title = models.CharField(max_length=100, verbose_name='Title')
-    slug = models.SlugField(max_length=100, verbose_name='Url', unique=True)
-    content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='was created')
-    
+class Category(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Title')
+    slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
+
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'slug':self.slug})
     
-    def get_ansolute_url(self):
-        return reverse('post', kwargs={'slug':self.slug})
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ['title']
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Title')
+    slug = models.SlugField(max_length=50, verbose_name='Url', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('tag', kwargs={'slug':self.slug})
+
+    class Meta:
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+        ordering = ['title']
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Title')
+    slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
+    author = models.CharField(max_length=100, verbose_name='Author')
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='was created')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    views = models.IntegerField(default=0, verbose_name='Amount views')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts')
+    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         verbose_name = 'Article'
         verbose_name_plural = 'Articles'
-        ordering = ['title', ]
+        ordering = ['created_at']
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'slug':self.slug})
